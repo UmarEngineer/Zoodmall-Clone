9f9ea60fe054ccc11e31bf03128e4dda12652701
UmarEngineer <ohojimirzayev@gmail.com>
2022-08-29 17:13:14 +0500

    Change Top Auto products section's images

--- src/images/TopAutoProducts.js
+++ src/images/TopAutoProducts.js
@@ -1,12 +1,28 @@
 
-export const NeolineXcop = "https://zoodmall.com/cdn-cgi/image/w=500,fit=contain,f=auto/https://images.zoodmall.com/web/product/picture/28/21646428/165744455540310800081.jpeg"
-
-export const SignatureRadar = "https://zoodmall.com/cdn-cgi/image/w=500,fit=contain,f=auto/https://images.zoodmall.com/web/product/picture/45/21504845/165651326515299400640.jpeg"
-
-export const AutosignalMagicar = "https://zoodmall.com/cdn-cgi/image/w=500,fit=contain,f=auto/https://images.zoodmall.com/web/product/picture/92/20043292/165492214391039200097.jpeg"
-
-export const AutoMonitorTesla = "https://zoodmall.com/cdn-cgi/image/w=500,fit=contain,f=auto/https://images.zoodmall.com/cloudinary/86/0f9/https%253A%252F%252Fupload.zoodmall.com%252Fweb%252Fproduct%252Fpicture%252F21%252F19679021%252F165285875277588800813.jpeg"
-
+export const NeolineXcop = {
+    name: "Radar detektori Neoline X-COP 7700s",
+    totalPrice: "1745,000",
+    monthlyPrice: "436,250",
+    link: "https://zoodmall.com/cdn-cgi/image/w=500,fit=contain,f=auto/https://images.zoodmall.com/cloudinary/cc/ebb/https%253A%252F%252Fupload.zoodmall.com%252Fweb%252Fproduct%252Fpicture%252F81%252F20898481%252F165252322546120700474.png"
+}
+export const Delkor = {
+    name: "Avtomobil akkumulyatori Delkor, Koreya",
+    totalPrice: "482,000",
+    monthlyPrice: "120,500",
+    link: "https://zoodmall.com/cdn-cgi/image/w=500,fit=contain,f=auto/https://upload.zoodmall.com/web/product/picture/44/14948144/162625665687317000802.jpeg"
+}
+export const KumhoShinasi = {
+    name: "Avtomobil Shinasi Kumho Solus TA71 195/60/15  (Komplekt)",
+    totalPrice: "3,475,000",
+    monthlyPrice: "868,750",
+    link: "https://zoodmall.com/cdn-cgi/image/w=500,fit=contain,f=auto/https://upload.zoodmall.com/web/product/picture/59/14462759/162391820828733700650.jpeg"
+}
+export const Charmhoo = {
+    name: "Avtoshina Charmhoo (Gopro) 185/70/13 (Komplekt)",
+    totalPrice: "2,469,750",
+    monthlyPrice: "868,750",
+    link: "https://zoodmall.com/cdn-cgi/image/w=500,fit=contain,f=auto/https://upload.zoodmall.com/web/product/picture/35/13880735/162108504548353400512.jpeg"
+}
 
 
 
