bdb2f11a61b15e2fd3bf83f404121a93b3180d03
UmarEngineer <ohojimirzayev@gmail.com>
2022-10-05 18:12:41 +0500

    Add Info Section and responsive

---
+++ src/Components/InfoSection.js
@@ -0,0 +1,76 @@
+import './Styles/InfoSection.css'
+
+const InfoSection = () => {
+  return (
+    <div className="info-text">
+        <h1>ZoodMall: Hozir xarid qiling, pulini keyinroq to'lang</h1>
+        <p>
+            Onlayn-xaridlar - taraqqiyot tomonga dadil qadamlarning namoyonidir. Bu sotuvchilar uchun ham, xaridorlar uchun ham qulay: mijoz o’ziga kerakli mahsulotlarni qidirishda vaqt tejaydi. Kerakli mahsulotni tanlashga bir nechagina daqiqa ajratib, qimmat bo’lmagan taklifni topish mumkin va bunda xaridingis uyizga yetkazilib ham beriladi. Shveytsariyaning OrientSwiss kompaniyasi ZoodMall deb nomlangan B2C formatidagi savdo maydonchasi ilovasini va saytini ishga tushirish orqali onlayn-xaridlarni takomillashtirmoqda va yanada hammabop qilmoqda.
+        </p>
+        <h1>
+            ZoodMall orqali nimalarni topish mumkin? 
+        </h1>
+        <p>
+            Saytda deyarli hamma narsa bor. 30 mingdan ortiq sotuvchi 6 milliondan ortiq mahsulot turlarini taklif etadi va assortiment kun sayin yanada kengayib bormoqda. Mahsulotlarni Xitoydan olish mumkin, shuningdek, mahalliy ishbilarmonlarning takliflari ham mavjud. Savdo maydonchasida siz quyidagilarni topasiz:
+        </p>
+        <ul>
+            <li>hamyonbop va taniqli brendlardan smartfonlar va boshqa elektronika;</li>
+            <li>ayollar, erkaklar va bolalarning kiyim-kechaklari;</li>
+            <li>avtomobillar va mototexnika uchun ehtiyot qismlar va aksessuarlar;</li>
+            <li>kompyuter uchun qismlar;</li>
+            <li>uylarni ta'mirlash va jihozlash uchun barcha mahsulotlar;</li>
+            <li>bolalar uchun mahsulotlar;</li>
+            <li>turli xil maishiy texnika;</li>
+            <li>aksessuarlar;</li>
+            <li>qo'lda ishlatiladigan va elektr asboblar;</li>
+            <li>barcha fasllar uchun poyabzal;</li>
+            <li>kanselyariya mahsulotlari;</li>
+            <li>atir-upalar va parvarish uchun kosmetika mahsulotlari;</li>
+            <li>zargarlik buyumlari;</li>
+            <li>sport mahsulotlari va b.</li>
+        </ul>
+        <p>
+            Bundan tashqari, ZoodMall maydonchasida siz:
+        </p>
+        <p>
+            <ul>
+                <li>Xiaomi;</li>
+                <li>Bosch;</li>
+                <li>Artel;</li>
+                <li>Nokia;</li>
+                <li><span style={{fontSize: '10.5pt'}}><span>Blackview</span></span>;</li>
+                <li>Apple;</li>
+                <li>Samsung;</li>
+                <li>Huawei va b.</li>
+            </ul>
+        </p>
+        <p>
+            kabi mashhur brendlardan mahsulotlarni arzon narxlarda topishingiz mumkin.
+        </p>
+        <p>
+            Ilova yoki veb-saytni ochar ekansiz, Xitoy va Ipak yo'li mamlakatlarini o'z ichiga olgan eng yirik bozorga tashrif buyurgan bo’lasiz.
+        </p>
+        <h1>
+            Buyurtma uchun to’lov qanday amalga oshiriladi va u qayerga yetkazib beriladi? 
+        </h1>
+        <p>
+            ZoodMall maydonchasida o’zizga yoqqan mahsulotni xarid qilgandan so'ng, "Hozir xarid qiling, pulini keyinroq to'lang" foizsiz bo’lib-bo’lib to'lash xizmatidan foydalanishingiz mumkin. ZoodPay tizimi sizga xaridlarni 4 yoki 6 ta teng to'lov orqali bo’lib-bo’lib to’lash imkoniyatini ta’minlaydi. Kerakli pul miqdorini o’tkazish kerakligini eslatish uchun sizga vaqtida oldindan xabarnomalar keladi. Bo’lib-bo’lib to’lashni milliy valyutada tanlashingiz mumkin, mahsilotlarning narxi sotib olish paytida qat’iy belgilanadi va keyinchalik o'zgarmaydi.
+        </p>
+        <p>
+            Bundan tashqari, «Yetkazib bergandan keyin to'lash» xizmati ham mavjud. Bunda kerakli pul miqdorini 14 kun ichida hech qanday foizsiz otkazishingiz kerak. ZoodMall maydonchasida mahsulotga buyurtma berish vaqtida siz pochtada yoki yetkazuvchi shaxsga naqd pul to’lashni tanlashingiz mumkin.
+        </p>
+        <p>
+            Yetkazib berish Qozog'iston, Ozarbayjon, O'zbekiston, shuningdek, Markaziy Osiyo va Yaqin Sharqning boshqa mamlakatlari bo’ylab amalga oshiriladi.
+        </p>
+        <h1>
+            Nafaqat xarid olish, balki sotish imkoni ham 
+        </h1>
+        <p>
+            ZoodMall maydonchasi ishga tushirilgan mamlakatlardagi mahalliy sotuvchilar, savdo maydonchasi imkoniyatlaridan foydalanish orqali, o'zlarining B2C  bizneslarini yo’lga qo’yishlari mumkin. O’z mahsulotlaringizni hamda ularning sifatli tavsifi va fotosuratlarini joylashtirish orqali, mahsulotlaringizni 225 milliondan ortiq mijozlar e’tiboriga havola qilish imkonini qo’lga kiritasiz. Mahsulot tavsiflari avtomatik ravishda 8 tilga tarjima qilinadi. Servis komissiyasi 2 dan 15% gachani tashkil etadi. Shuningdak, har doim qo'llab-quvvatlash xizmati ta’minlanadi.
+        </p>
+
+    </div>
+  )
+}
+
+export default InfoSection
